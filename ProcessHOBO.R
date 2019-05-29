c0b3e7777c2036ea79597c70878fda987c9ef30f
Crocker <daniel.crocker@mass.gov>
2019-05-29 15:31:45 -0400

    Fixed well stick-up offset according to field check on 5-28-2019

--- ProcessHOBO.R
+++ ProcessHOBO.R
@@ -404,12 +404,12 @@ df2$Stage_ft <- round(df2$raw_stage + offset, digits = 3)
 ### Source the function to calculate discharges
 source("HOBO_calcQ.R")
 if(loc == "SYW177"){
-  ### Well depth from casing top to bottom = 26.90625, stick-up height (2.2 + WLM calibration adjustment) = 2.21 ft 
+  ### Well depth from casing top to bottom = 26.90625, stick-up height (2.0 + WLM calibration adjustment) = 2.01 ft 
   hobo_tbl <- "tbl_HOBO_WELLS"
   df_HOBO <- df2 %>% 
   mutate("RatingFlag" = NA,
          "Discharge_cfs" = NA,
-         "Water_Level_ft" = round(26.90625 - 2.21 - Stage_ft, 2)) # Water level below ground surface
+         "Water_Level_ft" = round(26.90625 - 2.01 - Stage_ft, 2)) # Water level below ground surface
   print(head(hobo_tbl))
   } else {
   ### Calcualte all discharges and save df2 to a new df with discharge info
