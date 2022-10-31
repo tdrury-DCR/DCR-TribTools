84670efe074726938079f2676cb8a278b0eef07e
Crocker <daniel.crocker@mass.gov>
2022-10-31 13:15:56 -0400

    fix cleanings similar to prior two commits

--- mod_mayfly_correct.R
+++ mod_mayfly_correct.R
@@ -377,9 +377,9 @@ MF_CORRECT <- function(input, output, session, db_hobo, db_mayfly, df_fp, df_tri
   cleanings <- reactive({
     req(loc_selected())
     mf_cleanings() %>%
-      dplyr::filter(substrRight(Location, 4) == loc_selected()) %>%
-      select(c(3,10,28)) %>%
-      mutate(DateTimeUTC = as_datetime(glue("{FieldObsDate} {Mayfly_DownloadTimeUTC}"))) %>% 
+      dplyr::filter(substrRight(Location, 4) == loc) %>% 
+      rowwise() %>% 
+      mutate(DateTimeUTC = as_datetime(if(is.na(Mayfly_DownloadTimeUTC)) paste0(FieldObsDate, " ", HOBO_DownloadTimeUTC) else paste0(FieldObsDate, " ", Mayfly_DownloadTimeUTC))) %>%
       arrange(DateTimeUTC)
   })
   
