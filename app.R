5c38449c0d330de26f49ecb6e8a8b61ee8d4e9b6
Drury <travis.drury@mass.gov>
2026-08-12 09:30:00 -0400

    Styled files with Styler to tidyverse style. Fixed outlook email issue, was failing to cream COM object.

--- app.R
+++ app.R
@@ -3,10 +3,10 @@
 #  DESCRIPTION: Shiny web app to generate rating curves
 #  AUTHOR(S): Dan Crocker
 #  DATE LAST UPDATED: 2020-12-30
-#  GIT REPO: 
+#  GIT REPO:
 #  R version 3.5.3 (2019-03-11)  x86_64
 #  File Dependencies: Hobo_Rating_Configs.csv, LaunchTribTOOLS.R
-##############################################################################.
+############################################################################## .
 
 ### edit r environmental variables to avoid hardcoding UN and PW
 # Uncomment the line below to add ARCGIS_USER and ARCGIS_PASSWORD to your system variable list
@@ -15,28 +15,31 @@
 # Note: The above text do not have quotes, however when you use them below in the token, you do use quotes.
 
 ### LOAD CONFIGS FOR WAVE-WIT APPS ####
-username <- Sys.getenv('USERNAME')
-eq_team_root <- paste0('C:/Users/',username,'/Commonwealth of Massachusetts/DCR-TEAMS-DWSPEQ - Documents/')
+username <- Sys.getenv("USERNAME")
+eq_team_root <- paste0("C:/Users/", username, "/Commonwealth of Massachusetts/DCR-TEAMS-DWSPEQ - Documents/")
 
 # Function to load packages, and install if missing
-ipak <- function(pkg){
+ipak <- function(pkg) {
   new.pkg <- pkg[!(pkg %in% installed.packages(lib.loc = r_lib)[, "Package"])]
-  if (length(new.pkg))
-      install.packages(new.pkg, lib = r_lib, dependencies = TRUE, repos = "https://cloud.r-project.org")
+  if (length(new.pkg)) {
+    install.packages(new.pkg, lib = r_lib, dependencies = TRUE, repos = "https://cloud.r-project.org")
+  }
   si <- sessionInfo()
   loaded_pkgs <- names(si[["otherPkgs"]])
-  pkg <- pkg[!pkg %in% loaded_pkgs]  
+  pkg <- pkg[!pkg %in% loaded_pkgs]
   sapply(pkg, require, character.only = TRUE)
 }
 ### Package List ####
 ### NOTE - Shiny must be installed and loaded in the LaunchAppGitHub.R script - any other packages requred should be listed below
 
-packages <- c("DBI", "odbc","shiny","shinyjs", "tidyverse", "lubridate", "DT", "naniar", "shinyWidgets", "magrittr", "xts", "shinyTime",
-              "plotly",  "scales", "stringr", "shinythemes", "nlstools", "readxl", "shinycssloaders", "glue", "RDCOMClient", "dygraphs", 
-              "pool", "logging")
-ipak(packages) 
+packages <- c(
+  "DBI", "odbc", "shiny", "shinyjs", "tidyverse", "lubridate", "DT", "naniar", "shinyWidgets", "magrittr", "xts", "shinyTime",
+  "plotly", "scales", "stringr", "shinythemes", "nlstools", "readxl", "shinycssloaders", "glue", "RDCOMClient", "dygraphs",
+  "pool", "logging"
+)
+ipak(packages)
 
-substrRight <<- function(x, n){
+substrRight <<- function(x, n) {
   substr(x, nchar(x) - n + 1, nchar(x))
 }
 
@@ -44,7 +47,7 @@ substrRight <<- function(x, n){
 
 # options(shiny.error = function() {
 #   logging::logerror(sys.calls() %>% as.character %>% paste(collapse = ", ")) })
-  
+
 # options(shiny.error = browser)
 # options(shiny.error = recover)
 
@@ -58,10 +61,10 @@ if (userlocation == "Wachusett") {
   rootdir <- quab_team_root
 }
 
-#Set user info
-user <-  Sys.getenv("USERNAME") %>% toupper()
+# Set user info
+user <- Sys.getenv("USERNAME") %>% toupper()
 userdata <- readxl::read_xlsx(path = paste0(eq_team_root, config[["Users"]]))
-userdata <- userdata[toupper(userdata$Username) %in% user,]
+userdata <- userdata[toupper(userdata$Username) %in% user, ]
 username <<- paste(userdata[2], userdata[1], sep = " ")
 userlocation <<- paste0(userdata[6])
 
@@ -69,16 +72,16 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   schema <- "Wachusett"
   ### Connect to the DWSP database in SQL Server
 
-  dsn <- 'DCR_DWSP_App_R'
+  dsn <- "DCR_DWSP_App_R"
   database <- "DCR_DWSP"
-  tz <- 'America/New_York'
+  tz <- "America/New_York"
   tz_out <- tz
-  con <<- dbConnect(odbc::odbc(), dsn = dsn, uid = dsn, pwd = config[["DB Connection PW"]], timezone = tz, timezone_out =  tz_out)
+  con <<- dbConnect(odbc::odbc(), dsn = dsn, uid = dsn, pwd = config[["DB Connection PW"]], timezone = tz, timezone_out = tz_out)
 
-    ### DISCHARGE CALCULATOR Args
+  ### DISCHARGE CALCULATOR Args
   location_table <- config[["LocationsTable"]]
   df_locs <- dbReadTable(con, Id(schema = schema, table = location_table))
-  
+
   ### RATING TOOL Function Args
   measurement_data <- config[["DischargeTable"]] ### Set the table name with discharges
   rating_data <- config[["RatingsTable"]] ### Get the rating information
@@ -88,15 +91,17 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   df_trib_monitoring <- tbl(con, Id(schema = schema, table = "tblTributaryFieldNotes"))
   df_trib_monitoring <- df_trib_monitoring %>%
     select(-"Edit_timestamp") %>%
-    collect() 
-  
+    collect()
+
   df_trib_monitoring <- df_trib_monitoring %>%
     dplyr::arrange(desc(FieldObsDate))
 
   db_fp <- tbl(con, Id(schema = schema, table = "tblTribFieldParameters"))
   df_fp <- db_fp %>%
-    filter(DateTimeET > as_date("2024-01-01"),
-           Parameter %in% c("Staff Gauge Height", "Specific Conductance")) %>%
+    filter(
+      DateTimeET > as_date("2024-01-01"),
+      Parameter %in% c("Staff Gauge Height", "Specific Conductance")
+    ) %>%
     select(3:7) %>%
     collect()
 
@@ -104,9 +109,9 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   rm(con)
 
   # Then convert time tz and the format into UTC
-  df_fp <- df_fp |> 
+  df_fp <- df_fp |>
     mutate("DateTimeUTC" = with_tz(df_fp$DateTimeET, tz = "UTC"))
-  
+
   ### HOBO TOOL Function Args
   hobo_path <<- paste0(rootdir, config[["HOBO_Imported"]])
   updir <<- paste0(rootdir, config[["HOBO_Staging"]])
@@ -119,7 +124,7 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   mayfly_data_processed <<- paste0(rootdir, config[["Mayfly_Imported"]])
   mayfly_table <<- config[["Mayfly Table"]]
   emaillist <<- config[["Email_List"]]
-  
+
   ### Source Modules and functions
   source("fun_mod_hobos.R")
   source("mod_ratings.R")
@@ -135,66 +140,79 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   ### UI  ####
   ### font-family: 'Lobster', cursive;
   # shinythemes::themeSelector(),
-  ui <-  navbarPage(
+  ui <- navbarPage(
     "DCR-DWSP TRIB TOOLS",
-    tabPanel("HOBO/MAYFLY",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Tributary Sensor Data Tool"),
-                       HOBO_UI("mod_hobos"))
-    ),    tabPanel("RATINGS",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Tributary Rating Tool"),
-                       RATINGS_UI("mod_ratings"))
+    tabPanel(
+      "HOBO/MAYFLY",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Tributary Sensor Data Tool"),
+        HOBO_UI("mod_hobos")
+      )
+    ), tabPanel(
+      "RATINGS",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Tributary Rating Tool"),
+        RATINGS_UI("mod_ratings")
+      )
     ),
-    tabPanel("MAYFLY DATA CORRECTION",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Mayfly Data Correction Tools"),
-                       actionButton("refresh", "REFRESH"),
-                       br(),
-                       MF_CORRECT_UI("mod_mayfly_correct"))
+    tabPanel(
+      "MAYFLY DATA CORRECTION",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Mayfly Data Correction Tools"),
+        actionButton("refresh", "REFRESH"),
+        br(),
+        MF_CORRECT_UI("mod_mayfly_correct")
+      )
     ),
-    tabPanel("DISCHARGE CALCULATOR",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Manual Discharge Calculator"),
-                       DISCHARGECALC_UI("mod_dischargecalc"))
+    tabPanel(
+      "DISCHARGE CALCULATOR",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Manual Discharge Calculator"),
+        DISCHARGECALC_UI("mod_dischargecalc")
+      )
     )
-    
   ) ### END UI ####
-  
+
   ### SERVER  ####
   server <- function(input, output, session) {
     ### Setup parameters
     options(scipen = 999)
     callModule(RATINGS, "mod_ratings", df_discharges = df_discharges, df_ratings = df_ratings)
-    callModule(HOBO, "mod_hobos", hobo_path = hobo_path, updir = updir, hobo_db = hobo_db, df_trib_monitoring = df_trib_monitoring,
-               baro_table = baro_table, hobo_table = hobo_table, mayfly_data_dir = mayfly_data_dir,
-               mayfly_data_processed = mayfly_data_processed, ImportFlagTable = ImportFlagTable, username = username, userlocation = userlocation)
-    callModule(MF_CORRECT, "mod_mayfly_correct", df_fp = df_fp, 
-               df_trib_monitoring = df_trib_monitoring, username, userlocation)
+    callModule(HOBO, "mod_hobos",
+      hobo_path = hobo_path, updir = updir, hobo_db = hobo_db, df_trib_monitoring = df_trib_monitoring,
+      baro_table = baro_table, hobo_table = hobo_table, mayfly_data_dir = mayfly_data_dir,
+      mayfly_data_processed = mayfly_data_processed, ImportFlagTable = ImportFlagTable, username = username, userlocation = userlocation
+    )
+    callModule(MF_CORRECT, "mod_mayfly_correct",
+      df_fp = df_fp,
+      df_trib_monitoring = df_trib_monitoring, username, userlocation
+    )
     callModule(DISCHARGE, "mod_dischargecalc", df_ratings = df_ratings, df_locs = df_locs, userlocation)
     # Stop app when browser session window closes
     session$onSessionEnded(function() {
       stopApp()
-    })    
-    
-  } # End Server 
-  
+    })
+  } # End Server
 } else { ### QUABBIN ####
- 
-  ### Connect to the DWSP database in SQL Server 
+
+  ### Connect to the DWSP database in SQL Server
   database <- config[["HOBO_DB"]]
   ### Connect to Database #1
   ### Connect to the DWSP database in SQL Server
   schema <- "Quabbin"
-  dsn <- 'DCR_DWSP_App_R'
+  dsn <- "DCR_DWSP_App_R"
   database <- "DCR_DWSP"
-  tz <- 'UTC'
+  tz <- "UTC"
   con <- dbConnect(odbc::odbc(), dsn = dsn, uid = dsn, pwd = config[["DB Connection PW"]], timezone = tz)
-  
+
   ### DISCHARGE CALCULATOR Args
   location_table <- config[["LocationsTable"]]
   df_locs <- dbReadTable(con, Id(schema = schema, table = location_table))
-  
+
   ### RATING TOOL Function Args
   measurement_data <- config[["DischargeTable"]] ### Set the table name with discharges
   rating_data <- config[["RatingsTable"]] ### Get the rating information
@@ -210,24 +228,26 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   df_trib_monitoring <- tbl(con, Id(schema = schema, table = "tblMayflyMaintenance"))
   df_trib_monitoring <- df_trib_monitoring %>%
     select(-"Edit_timestamp") %>%
-    collect() 
-  
+    collect()
+
   df_trib_monitoring <- df_trib_monitoring %>%
     rename(DateTimeUTC = DateTimeET) %>%
-    mutate(DateTimeUTC = with_tz(force_tz(DateTimeUTC, tzone="America/New_York"), tzone="UTC"),
-           Mayfly_DownloadTimeUTC = DateTimeUTC) %>%
+    mutate(
+      DateTimeUTC = with_tz(force_tz(DateTimeUTC, tzone = "America/New_York"), tzone = "UTC"),
+      Mayfly_DownloadTimeUTC = DateTimeUTC
+    ) %>%
     dplyr::arrange(desc(DateTimeUTC))
-  
+
   dbDisconnect(con)
   rm(con)
-  
+
   # First force the tz attribute to reflect the timezone that the data appears in
   df_fp$DateTimeET <- force_tz(df_fp$DateTimeET, tz = "America/New_York")
   # Then convert time tz and the format into UTC
   df_fp$DateTimeET <- with_tz(df_fp$DateTimeET, tz = "UTC")
   df_fp <- df_fp %>%
     dplyr::rename(DateTimeUTC = DateTimeET)
-  
+
   ### HOBO TOOL Function Args
   hobo_path <<- paste0(rootdir, config[["HOBO_Imported"]])
   updir <<- paste0(rootdir, config[["HOBO_Staging"]])
@@ -240,7 +260,7 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   mayfly_data_processed <<- paste0(rootdir, config[["Mayfly_Imported"]])
   mayfly_table <<- config[["Mayfly Table"]]
   emaillist <<- config[["Email_List"]]
-  
+
   ### Source Modules and functions
   source("mod_ratings.R")
   source("Ratings.R")
@@ -255,52 +275,64 @@ if (userlocation == "Wachusett") { ### WACHUSETT ####
   source("fun_mayfly_correct.R")
   source("mod_dischargecalc.R")
   # source("ProcessMayflyData.R")
-  
-  ui <-  navbarPage(
+
+  ui <- navbarPage(
     "DCR-DWSP TRIB TOOLS",
-    tabPanel("MAYFLY/HOBO",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Tributary Sensor Data Tool"),
-                       HOBO_UI("mod_hobos"))
-    ),    tabPanel("RATINGS",
-                   fluidPage(theme = shinytheme("united"),
-                             h1("Tributary Rating Tool"),
-                             RATINGS_UI("mod_ratings"))
+    tabPanel(
+      "MAYFLY/HOBO",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Tributary Sensor Data Tool"),
+        HOBO_UI("mod_hobos")
+      )
+    ), tabPanel(
+      "RATINGS",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Tributary Rating Tool"),
+        RATINGS_UI("mod_ratings")
+      )
     ),
-    tabPanel("MAYFLY DATA CORRECTION",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Mayfly Data Correction Tools"),
-                       actionButton("refresh", "REFRESH"),
-                       br(),
-                       MF_CORRECT_UI("mod_mayfly_correct"))
+    tabPanel(
+      "MAYFLY DATA CORRECTION",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Mayfly Data Correction Tools"),
+        actionButton("refresh", "REFRESH"),
+        br(),
+        MF_CORRECT_UI("mod_mayfly_correct")
+      )
     ),
-    tabPanel("DISCHARGE CALCULATOR",
-             fluidPage(theme = shinytheme("united"),
-                       h1("Manual Discharge Calculator"),
-                       DISCHARGECALC_UI("mod_dischargecalc"))
+    tabPanel(
+      "DISCHARGE CALCULATOR",
+      fluidPage(
+        theme = shinytheme("united"),
+        h1("Manual Discharge Calculator"),
+        DISCHARGECALC_UI("mod_dischargecalc")
+      )
     )
-    
-    
   ) ### END UI ####
-  
+
   ### SERVER  ####
   server <- function(input, output, session) {
     ### Setup parameters
     options(scipen = 999)
     callModule(RATINGS, "mod_ratings", df_discharges = df_discharges, df_ratings = df_ratings)
-    callModule(HOBO, "mod_hobos", hobo_path = hobo_path, updir = updir, hobo_db = hobo_db, df_trib_monitoring = df_trib_monitoring,
-               baro_table = baro_table, hobo_table = hobo_table, mayfly_data_dir = mayfly_data_dir,
-               mayfly_data_processed = mayfly_data_processed, ImportFlagTable = ImportFlagTable, username = username, userlocation = userlocation)
-    callModule(MF_CORRECT, "mod_mayfly_correct", df_fp = df_fp, 
-               df_trib_monitoring = df_trib_monitoring, username, userlocation)
+    callModule(HOBO, "mod_hobos",
+      hobo_path = hobo_path, updir = updir, hobo_db = hobo_db, df_trib_monitoring = df_trib_monitoring,
+      baro_table = baro_table, hobo_table = hobo_table, mayfly_data_dir = mayfly_data_dir,
+      mayfly_data_processed = mayfly_data_processed, ImportFlagTable = ImportFlagTable, username = username, userlocation = userlocation
+    )
+    callModule(MF_CORRECT, "mod_mayfly_correct",
+      df_fp = df_fp,
+      df_trib_monitoring = df_trib_monitoring, username, userlocation
+    )
     callModule(DISCHARGE, "mod_dischargecalc", df_ratings = df_ratings, df_locs = df_locs, userlocation)
     # Stop app when browser session window closes
     session$onSessionEnded(function() {
       stopApp()
-    })    
-    
-  } # End Server 
-  
+    })
+  } # End Server
 }
 
 # combines the user interface and server
